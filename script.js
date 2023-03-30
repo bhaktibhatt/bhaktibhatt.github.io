d444c23c02b35220d042c4d37f0dcf01d12daf6a
Bhakti <bhaktinirbhaybhatt@gmail.com>
2023-03-30 12:01:28 +0530

    navbar added to home page but one svg not being idk why tf🥲

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
-const observer = new IntersectionObserver((entries) =>{
-    entries.forEach((entry) => {
-        console.log(entry)
-        if(entry.isIntersecting){
-            entry.target.classList.add('show');
-        }
-        else{
-            entry.target.classList.remove('show');
-        }
-    });
-});
-const logoMove = document.getElementsByClassName('space-cat-pink')
-logoMove.observer.observe;
+const nav =  document.querySelector('.navbar')
+fetch('/navbar.html')
+.then(res=>res.text())
+.then(data=>{
+    nav.innerHTML = data
+})
