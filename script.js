c009ab094f95d2c374346d7a1dbaaabfdf59f670
Bhakti <bhaktinirbhaybhatt@gmail.com>
2023-03-30 12:27:04 +0530

    minor changes in few files and navbar stitching not working🥲

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const nav =  document.querySelector('.navbar')
 fetch('/navbar.html')
-.then(res=>res.text())
+.then(res=>res.text()) //convert response of promise to text
 .then(data=>{
     nav.innerHTML = data
 })
