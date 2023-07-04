cda36ae0c85be782d81f73ae0eb8fde129f4adaa
Bhakti <bhaktinirbhaybhatt@gmail.com>
2023-07-04 20:45:10 +0530

    responsive navbar

--- script.js
+++ script.js
@@ -4,8 +4,15 @@
 // .then(data=>{
 //     nav.innerHTML = data
 // })
-const hamMenu = document.querySelectorAll('.ham');
-const mobileMenu = document.querySelectorAll(".mobile-nav");
-hamMenu.addEventListener('click',function (){
-    mobileMenu.classList.toggle('is-active');
+// const hamMenu = document.querySelectorAll('.ham');
+// const mobileMenu = document.querySelectorAll(".mobile-nav");
+// hamMenu.addEventListener('click',function (){
+//     mobileMenu.classList.toggle('is-active');
+// })
+const hamBtn = document.querySelector(".ham");
+const mobileMenu = document.querySelector(".mobile-nav");
+console.log(mobileMenu)
+hamBtn.addEventListener('click', function(){
+    console.log("ham clicked")
+    mobileMenu.classList.toggle("is-active")
 })
